6c943803ca3065575ef9d802ca438c292f83b998
Todd Hodes <thodes@wish.com>
2021-09-12 15:43:42 -0700

    init AppsFlyer

--- app/src/main/java/com/example/imagesearchapp/ImageSearchApplication.kt
+++ app/src/main/java/com/example/imagesearchapp/ImageSearchApplication.kt
@@ -2,6 +2,16 @@ package com.example.imagesearchapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import com.appsflyer.AppsFlyerLib;
+
 
 @HiltAndroidApp
-class ImageSearchApplication : Application()
+class ImageSearchApplication : Application() {
+
+   private val AF_DEV_KEY = ""
+
+    override fun onCreate() {
+        super.onCreate()
+        AppsFlyerLib.getInstance().init(AF_DEV_KEY, null, this);
+    }
+}
