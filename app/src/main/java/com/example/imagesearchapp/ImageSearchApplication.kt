8844452d9ebc66180e6b6aa6ca0eaa597bc9a339
Todd Hodes <thodes@wish.com>
2021-09-12 17:25:11 -0700

    rm google crap

--- app/src/main/java/com/example/imagesearchapp/ImageSearchApplication.kt
+++ app/src/main/java/com/example/imagesearchapp/ImageSearchApplication.kt
@@ -7,7 +7,7 @@ import com.appsflyer.AppsFlyerLib
 @HiltAndroidApp
 class ImageSearchApplication : Application() {
 
-    private val AF_DEV_KEY = ""
+    private val AF_DEV_KEY = "xTZBvkNLiBuWdH86YzeTsC"
 
     override fun onCreate() {
         super.onCreate()
@@ -18,33 +18,5 @@ class ImageSearchApplication : Application() {
         // start
         AppsFlyerLib.getInstance().start(this)
         AppsFlyerLib.getInstance().setDebugLog(true);
-
-        googleInstallReferrer()
-    }
-
-    //private lateinit var referrerClient: InstallReferrerClient
-    private fun googleInstallReferrer() {
-//        referrerClient = InstallReferrerClient.newBuilder(this).build()
-//        referrerClient.startConnection(object : InstallReferrerStateListener {
-//
-//            override fun onInstallReferrerSetupFinished(responseCode: Int) {
-//                when (responseCode) {
-//                    InstallReferrerResponse.OK -> {
-//                        // Connection established.
-//                    }
-//                    InstallReferrerResponse.FEATURE_NOT_SUPPORTED -> {
-//                        // API not available on the current Play Store app.
-//                    }
-//                    InstallReferrerResponse.SERVICE_UNAVAILABLE -> {
-//                        // Connection couldn't be established.
-//                    }
-//                }
-//            }
-//
-//            override fun onInstallReferrerServiceDisconnected() {
-//                // Try to restart the connection on the next request to
-//                // Google Play by calling the startConnection() method.
-//            }
-//        })
     }
 }
