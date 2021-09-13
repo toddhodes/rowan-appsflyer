e867813268f83645fedfafd0a91afe175d7e4d1f
Todd Hodes <thodes@wish.com>
2021-09-12 17:50:22 -0700

    add pkg logging

--- app/src/main/java/com/example/imagesearchapp/ImageSearchApplication.kt
+++ app/src/main/java/com/example/imagesearchapp/ImageSearchApplication.kt
@@ -1,6 +1,7 @@
 package com.example.imagesearchapp
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
 import com.appsflyer.AppsFlyerLib
 
@@ -11,6 +12,9 @@ class ImageSearchApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        val pkg = this.applicationContext.packageName
+        Log.i("init", "pkg: $pkg")
 
         // init
         AppsFlyerLib.getInstance().init(AF_DEV_KEY, null, this);
