4d2de4a84535b6724d69239fd14e2324f7d05c34
Todd Hodes <thodes@wish.com>
2021-09-12 18:44:23 -0700

    track a purchase

--- app/src/main/java/com/example/imagesearchapp/ui/main/PhotoDetailFragment.kt
+++ app/src/main/java/com/example/imagesearchapp/ui/main/PhotoDetailFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +18,10 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.example.imagesearchapp.R
 import com.example.imagesearchapp.databinding.FragmentPhotoDetailBinding
+import com.appsflyer.AppsFlyerLib
+import com.appsflyer.AFInAppEventType // Predefined event names
+import com.appsflyer.AFInAppEventParameterName // Predefined parameter names
+import com.appsflyer.attribution.AppsFlyerRequestListener
 
 /**
  * A simple [Fragment] subclass.
@@ -68,8 +73,42 @@ class PhotoDetailFragment : Fragment(R.layout.fragment_photo_detail) {
 
             tvUsername.text = photo.user.name
             tvDesc.text = photo.desc
-        }
 
+            // track af_purchase.af_revenue
+            trackPurchase(photo.user.name)
+        }
+    }
+
+    private fun trackPurchase(name: String) {
+        // use the following to fake a purchase:
+        //
+        //logEvent(Context context,
+        //         java.lang.String eventName,
+        //         java.util.Map<java.lang.String,java.lang.Object> eventValues,
+        //         AppsFlyerRequestListener listener)
+
+        val params = mapOf(
+                AFInAppEventParameterName.REVENUE to 9.99,
+                AFInAppEventParameterName.PRICE to 8.88,
+                AFInAppEventParameterName.CONTENT_ID to name
+            )
+
+        AppsFlyerLib.getInstance().logEvent(
+            this.requireContext(),
+            AFInAppEventType.PURCHASE,
+            params,
+            object : AppsFlyerRequestListener {
+                override fun onSuccess() {
+                    Log.d("event", "Event sent successfully $params")
+                }
+                override fun onError(errorCode: Int, errorDesc: String) {
+                    Log.d(
+                        "event",
+                        "Event failed to be sent, Error code: $errorCode description: $errorDesc"
+                    )
+                }
+            }
+        )
 
     }
 }
